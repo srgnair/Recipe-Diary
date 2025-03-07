32d2f759223b73d0c59f5ca12d40aa39d26b45d7
白金愛里 <air.shirogane@gmail.com>
2025-03-08 08:38:57 +0900

    delete unwanted comments

--- src/resources/views/top.blade.php
+++ src/resources/views/top.blade.php
@@ -3,7 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="description" content="料理レシピを公開するサイトです。">
-    <!-- 検索結果の説明文 -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="img/favicon.ico">
     <link rel="stylesheet" href="{{ asset('css/top.css') }}" />
